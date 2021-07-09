715330a89f60e7117f503baf08d35a45ee689f93
EllieAzaveda <elliemiller66@icloud.com>
2021-07-09 17:40:02 -0600

    Move App Components to folder

---
+++ src/Components/App/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+// import { client } from "./../ApolloClient/client";
+// import { ApolloProvider } from '@apollo/client';
+// import { Route, Switch, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    // {<ApolloProvider client={client}>}
+      <div className="App">
+        <Header />
+        <Switch>
+          {/* <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home/:id">
+            <Home />
+          </Route>
+          <Route path="/pack/:id">
+            <Pack />
+          </Route> */}
+        </Switch>
+      </div>
+    // {</ApolloProvider>}
+  );
+}
+
+export default App;
