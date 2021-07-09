0449065a39e50aa4d9709af7e1c3c418430ae5f2
Zach Johnson <zachjjohns@gmail.com>
2021-07-09 13:07:34 -0500

    Add placeholder Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import { client } from "./ApolloClient/client";
 import { ApolloProvider } from '@apollo/client';
+// import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <Header />
-        
+        {/* <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home/:id">
+            <Home />
+          </Route>
+          <Route path="/pack/:id">
+            <Pack />
+          </Route>
+        </Switch> */}
       </div>
     </ApolloProvider>
   );
