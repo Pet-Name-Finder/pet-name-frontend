35b6c900bea42f15980007c8ba7e4a5515bc171e
Ryan Miller <ryandmiller3521@gmail.com>
2021-07-10 09:19:24 -0600

    Add mapped array of PackCards

--- src/Components/ViewPacks/ViewPacks.js
+++ src/Components/ViewPacks/ViewPacks.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './ViewPacks.css';
 import data from '../../Data/Pack';
+import PackCard from '../PackCard/PackCard';
 //temporary place for data while we decide we are making the api call for this data
 
 const ViewPacks = () => {
-    
+    const displayCards = data.packs.map(pack => {
+        return (
+            <PackCard
+                pack={pack}
+                name={pack.name}
+                key={pack.id}
+            />
+        )
+    })
     return (
         <main className='packs-container'>
-            
+            {displayCards}
         </main>
     )
 }
