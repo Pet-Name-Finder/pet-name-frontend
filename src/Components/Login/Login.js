698dca1bd801e507648a493574e3ef09fe2f6a58
EllieAzaveda <elliemiller66@icloud.com>
2021-07-09 19:29:39 -0600

    Add basic login elements

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,7 +4,7 @@ import './Login.css';
 
 const Login = () => {
   return (
-    <NavLink to='/'>
+    <NavLink to='/login'>
       <div className='login'>
         <h1 className='login-header'>Pet Name Finder</h1>
         <input className='login-input' placeholder='Email'></input>
