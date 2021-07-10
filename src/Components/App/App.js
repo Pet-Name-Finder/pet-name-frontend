898356678a5ff5ea853dad3db845bfd436afc16c
EllieAzaveda <elliemiller66@icloud.com>
2021-07-10 16:35:48 -0600

    Add login email verification method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,18 +6,39 @@ import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Home from '../Home/Home';
 import ViewPacks from '../ViewPacks/ViewPacks';
+import LikedNames from '../LikedNames/LikedNames';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import userData from '../../Data/User';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
       this.state = {
-
+        allUsers: userData.users,
+        currentUser: null,
+        likedNames: [],
+        allPacks: [],
+        loggedIn: false
       }
   }
 
+  setUser = (user) => {
+    this.setState({ currentUser: user });
+    this.setState({ loggedIn: false })
+  }
+
+  setLikedNames = ({ currentUser }) => {
+    let foundLiked = currentUser.likedNames;
+    this.setState({ currentUser: foundLiked });
+  }
+
+  // componentDidMount() {
+  //
+  // }
+
   render() {
+    console.log("CURRENT USER", this.state.currentUser)
     return (
       <>
         <div className="App">
@@ -29,11 +50,19 @@ class App extends Component {
                 }}
               />
               <Route path="/login" render={() => {
-                return <Login />
+                return <Login
+                  setUser={this.setUser}
+                />
                 }}
               />
               <Route path="/all-packs" render={() => {
                 return <ViewPacks />
+                }}
+              />
+              <Route path="/liked-names" render={() => {
+                return <LikedNames
+                  likedNames={this.state.likedNames}
+                />
                 }}
               />
               <Route path="*" render={() => {
