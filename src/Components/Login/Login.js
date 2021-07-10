572e25cd8345212454bf4cce2e250e9cd977aa44
EllieAzaveda <elliemiller66@icloud.com>
2021-07-10 16:35:34 -0600

    Add login email verification method

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import userData from '../../Data/User';
 import './Login.css';
 
-const Login = () => {
+class Login extends Component {
+  constructor() {
+    super();
+      this.state = {
+        allUsers: userData.users,
+        emailInput: ""
+      }
+  }
+
+  handleChange = (event) => {
+    this.setState({ emailInput: event.target.value })
+  }
+
+  clearInput = () => {
+    this.setState({ emailInput: "" })
+  }
+
+  searchInput = (event) => {
+    event.preventDefault();
+    let foundUser = this.state.allUsers.find(user => {
+      return user.email === this.state.emailInput
+    });
+
+    if(!foundUser) {
+      alert("Sorry no account found!");
+    } else {
+      this.props.setUser(foundUser);
+    }
+    this.clearInput();
+  }
+
+  // componentDidMount() {
+  //
+  // }
+
+  render() {
   return (
-    <NavLink to='/login'>
-      <div className='login'>
-        <h1 className='login-header'>Pet Name Finder</h1>
-        <input className='login-input' placeholder='Email'></input>
-        <button>🐾 Login Now! 🐾</button>
-      </div>
-    </NavLink>
+    <div className="login">
+      <h1 className="login-header">Pet Name Finder</h1>
+      <input
+        className="login-input"
+        placeholder="Email"
+        value={this.state.searchInput}
+        onChange={(event) => this.handleChange(event)}
+      >
+      </input>
+      <NavLink to="/">
+        <button
+          onClick={(event) => this.searchInput(event)}>🐾 Login Now! 🐾
+        </button>
+      </NavLink>
+    </div>
   )
+  }
 }
 
 export default Login;
+
+//  currentUser: this.props.currentUser
