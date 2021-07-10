4a868e7d37d7e99263e71380bc649b1950e1c181
Zach Johnson <zachjjohns@gmail.com>
2021-07-10 10:56:04 -0500

    Create LikedNames folder/files

---
+++ src/Components/LikedNames/LikedNames.js
@@ -0,0 +1,22 @@
+mport './LikedNames.css';
+import React from 'react';
+
+export default function LikedNames({ likedNames }) {
+
+  const mappedNames = likedNames.map((name) => {
+    return (
+      <li>{name}</li>
+      //might need keys for each li element?
+      //will implement a delete/remove name button here once we figure out how we're passing state
+    )
+  })
+
+  return (
+    <>
+      <h2>Your Liked Names:</h2>
+      <ul>
+        {mappedNames}
+      </ul>
+    </>
+  )
+}
