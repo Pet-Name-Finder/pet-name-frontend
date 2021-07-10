bfdec3242fd27a7317dc56f2e27af5a0393b12f7
EllieAzaveda <elliemiller66@icloud.com>
2021-07-10 12:44:18 -0600

    Add route for view all packs

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,35 +1,3 @@
-import './App.css';
-<<<<<<< HEAD
-// import { client } from "./../ApolloClient/client";
-// import { ApolloProvider } from '@apollo/client';
-import { Route, Switch, Link } from 'react-router-dom';
-import Header from '../Header/Header';
-
-function App() {
-  return (
-    // {<ApolloProvider client={client}>}
-      <div className="App">
-        <Header />
-        {/* <div className='app-container'>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="/home/:id">
-            <Home />
-          </Route>
-          <Route path="/pack/:id">
-            <Pack />
-          </Route>
-        </Switch>
-        </div>*/}
-      </div>
-    // {</ApolloProvider>}
-  );
-}
-
-export default App;
-=======
 import React, { Component } from 'react';
 // import { client } from "./../ApolloClient/client";
 // import { ApolloProvider } from '@apollo/client';
@@ -38,8 +6,8 @@ import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Home from '../Home/Home';
 import ViewPacks from '../ViewPacks/ViewPacks';
-import Pack from '../Pack/Pack';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -64,13 +32,9 @@ class App extends Component {
                 return <Login />
                 }}
               />
-              <Route path="/viewPacks" render={() => {
+              <Route path="/all-packs" render={() => {
                 return <ViewPacks />
-              }}
-              />
-              <Route path="/pack:id" render={() => {
-                return <Pack />
-              }}
+                }}
               />
               <Route path="*" render={() => {
                 return <NotFoundPage />
@@ -95,4 +59,3 @@ export default App;
 // <Route path="/pack/:id">
 //   <Pack />
 // </Route>
->>>>>>> ef2d0ada8d701359be1c9afb034312306b946e30
