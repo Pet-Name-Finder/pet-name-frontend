7a901f7a43c783f1f71a626c9b9f40afe5e28e03
Ryan Miller <ryandmiller3521@gmail.com>
2021-07-10 09:30:02 -0600

    Add pack files and basic setup

---
+++ src/Components/Pack/Pack.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Pack.css';
+
+class Pack extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <main className="pack">
+
+            </main>
+        );
+    }
+}
+
+export default Pack;
