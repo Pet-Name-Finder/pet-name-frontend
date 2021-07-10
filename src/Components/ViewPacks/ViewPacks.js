a0682bdf3630f730833822ccd4022f86fc29a8ed
Ryan Miller <ryandmiller3521@gmail.com>
2021-07-10 09:04:55 -0600

    Add basic layout for pack component

---
+++ src/Components/ViewPacks/ViewPacks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ViewPacks.css';
+
+const ViewPacks = () => {
+    return (
+        <main className='packs-container'>
+
+        </main>
+    )
+}
+
+export default ViewPacks;
