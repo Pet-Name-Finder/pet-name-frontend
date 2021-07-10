de133e2851a8020ab724ab972530ab123d62e178
Ryan Miller <ryandmiller3521@gmail.com>
2021-07-10 10:35:47 -0600

    Add basic layout for pack component

--- src/Components/Pack/Pack.js
+++ src/Components/Pack/Pack.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './Pack.css';
+import data from '../../Data/Names';
 
 class Pack extends Component {
     constructor() {
         super();
         this.state = {
-
+            names: data.names,
+            currentName: 0,
         }
     }
 
     render() {
         return (
             <main className="pack">
-                <p>hello</p>
+                <button>Down Vote!</button>
+                <section>
+                    <p>{this.state.names[this.state.currentName].name}</p>
+                </section>
+                <button>Up Vote!</button>
             </main>
         );
     }
--- src/Data/Names.js
+++ src/Data/Names.js
@@ -62,3 +62,5 @@ const data = {
         },
     ]
 }
+
+export default data;
