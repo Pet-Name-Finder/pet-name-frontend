c5b0e2382e284c244ea27ff7057a745eaad9fa0d
EllieAzaveda <elliemiller66@icloud.com>
2021-07-09 18:21:44 -0600

    Add login input and button

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <NavLink to='/'>
+      <div className='login'>
+        <h1 className='login-header'>Pet Name Finder</h1>
+        <input className='login-input' placeholder='Email'></input>
+        <button>🐾 Login Now! 🐾</button>
+      </div>
+    </NavLink>
+  )
+}
+
+export default Login;
