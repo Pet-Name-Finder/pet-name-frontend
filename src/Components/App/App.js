88e18dc434ac474d29c11fa43db84f02554d70bf
EllieAzaveda <elliemiller66@icloud.com>
2021-07-09 18:20:08 -0600

    Add Routes for login and 404 error

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,30 +1,48 @@
 import './App.css';
+import React, { Component } from 'react';
 // import { client } from "./../ApolloClient/client";
 // import { ApolloProvider } from '@apollo/client';
 import { Route, Switch, Link } from 'react-router-dom';
 import Header from '../Header/Header';
+import Login from '../Login/Login';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
-function App() {
-  return (
-    // {<ApolloProvider client={client}>}
-      <div className="App">
-        <Header />
-        {/* <div className='app-container'>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="/home/:id">
-            <Home />
-          </Route>
-          <Route path="/pack/:id">
-            <Pack />
-          </Route>
-        </Switch>
-        </div>*/}
-      </div>
-    // {</ApolloProvider>}
-  );
+class App extends Component {
+  constructor() {
+    super();
+      this.state = {
+
+      }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <Header />
+          <div className='app-container'>
+            <Switch>
+              <Route exact path="/" render={() => {
+                <Login />
+                }}
+              />
+              <Route path="*" render={() => {
+                return <NotFoundPage />
+                }}
+              />
+            </Switch>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
+
+//  <Route path="/home/:id">
+//   <Home />
+// </Route>
+// <Route path="/pack/:id">
+//   <Pack />
+// </Route>
