b2eac08688fb6cd8134fac046657902b5c2282a8
EllieAzaveda <elliemiller66@icloud.com>
2021-07-11 13:25:08 -0600

    Add data cy tags

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,9 +13,9 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <NavLink to="/all-packs"><button className="main-btn view-packs-btn">View Your Packs</button></NavLink>
-        <button className="main-btn start-pack-btn">Start New Pack</button>
-        <button className="main-btn start-lone-btn">Start as a Lone Wolf</button>
+        <NavLink to="/all-packs"><button data-cy="view-packs-btn" className="main-btn view-packs-btn">View Your Packs</button></NavLink>
+        <button data-cy="start-pack-btn" className="main-btn start-pack-btn">Start New Pack</button>
+        <button data-cy="start-lone-btn" className="main-btn start-lone-btn">Start as a Lone Wolf</button>
       </div>
     );
   }
