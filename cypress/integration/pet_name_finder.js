eefed53f4a2f5188c3e470744ac5081f170c01c2
EllieAzaveda <elliemiller66@icloud.com>
2021-07-11 13:04:26 -0600

    Add before each and url test

---
+++ cypress/integration/pet_name_finder.js
@@ -0,0 +1,12 @@
+describe('Show main view of Pet Name Finder App', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+
+  it('Should be able to visit the main page', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
+
+
+});
