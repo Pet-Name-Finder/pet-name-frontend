fc3221608c6e68c9486b184d1d9f5a6640f62031
EllieAzaveda <elliemiller66@icloud.com>
2021-07-11 00:57:30 -0600

    Add all login functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import userData from '../../Data/User';
 import './Login.css';
 
-const Login = () => {
-  return (
-    <NavLink to='/login'>
-      <div className='login'>
-        <h1 className='login-header'>Pet Name Finder</h1>
-        <input className='login-input' placeholder='Email'></input>
-        <button>🐾 Login Now! 🐾</button>
+class Login extends Component {
+  constructor() {
+    super();
+      this.state = {
+        allUsers: userData.users,
+        emailInput: ""
+      }
+  }
+
+  handleChange = (event) => {
+    this.setState({ emailInput: event.target.value })
+  }
+
+  clearInput = () => {
+    this.setState({ emailInput: "" })
+  }
+
+  searchInput = (event) => {
+    event.preventDefault();
+    let foundUser = this.state.allUsers.find(user => {
+      return user.email === this.state.emailInput
+    });
+
+    if(!foundUser) {
+      alert("Sorry no account found!");
+      this.clearInput();
+    } else {
+      this.props.setUser(foundUser);
+    }
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <h1 className="login-header">Pet Name Finder</h1>
+        <input
+          className="login-input"
+          placeholder="Email"
+          value={this.state.searchInput}
+          onChange={(event) => this.handleChange(event)}
+        >
+        </input>
+        <NavLink to="/"><button
+            onClick={(event) => this.searchInput(event)}>🐾 Login Now! 🐾
+          </button></NavLink>
       </div>
-    </NavLink>
-  )
+    )
+  }
 }
 
 export default Login;
