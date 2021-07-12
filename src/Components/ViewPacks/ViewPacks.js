81fa9813d179663e9aafada349afa6af706ffe81
Zach Johnson <zachjjohns@gmail.com>
2021-07-11 20:27:03 -0500

    Add basic borders for PackCards

--- src/Components/ViewPacks/ViewPacks.js
+++ src/Components/ViewPacks/ViewPacks.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import './ViewPacks.css';
-import data from '../../Data/Pack';
-import PackCard from '../PackCard/PackCard';
+import React from "react";
+import "./ViewPacks.css";
+import data from "../../Data/Pack";
+import PackCard from "../PackCard/PackCard";
 //temporary place for data while we decide we are making the api call for this data
 
 const ViewPacks = ({ allPacks }) => {
   // Now we can add matching or something for just their specific packs
-    const displayCards = data.packs.map(pack => {
-        return (
-            <PackCard
-                pack={pack}
-                name={pack.name}
-                key={pack.id}
-            />
-        )
-    })
-    
-    return (
-        <main className='packs-container'>
-            {displayCards}
-        </main>
-    )
-}
+  const displayCards = data.packs.map((pack) => {
+    return <PackCard pack={pack} name={pack.name} key={pack.id} />;
+  });
+
+  return (
+    <main className="packs-container">
+      <h2>Your Packs</h2>
+      {displayCards}
+    </main>
+  );
+};
 
 export default ViewPacks;
