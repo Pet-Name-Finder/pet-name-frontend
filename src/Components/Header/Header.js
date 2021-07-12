b0343aeddd3f6a5180effe99703801e943162162
Zach Johnson <zachjjohns@gmail.com>
2021-07-11 19:36:03 -0500

    Change main-btn color

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,49 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = ({ loggedIn, logoutUser }) => {
   // Hamburger will be conditionally rendered for mobile view
   return (
-    <nav className='header'>
-      <div className='hamburger-menu'>
+    <nav className="header">
+      <div className="hamburger-menu">
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <NavLink to='/'><button data-cy='home-button' className='home-button'>Take Me Back Home!</button></NavLink>
-      <img src="./pet.png" alt="Pet Name Finder logo" className='logo'></img>
-      <h1 className='header-title'>Pet Name Finder</h1>
-      <NavLink to='/liked-names'><button data-cy='view-liked-button' className='view-liked-button'>⭐️ View Liked Names ⭐️</button></NavLink>
-      {!loggedIn &&
-      <NavLink to='/login'><button data-cy='login-button' className='login-button'>Login</button></NavLink>
-      }
-      {loggedIn &&
-      <button
-        data-cy='login-button'
-        className='login-button'
-        onClick={(event) => logoutUser(event)}
-      >Logout</button>
-      }
+      <NavLink to="/">
+        <button data-cy="home-button" className="home-button">
+          Take Me Back Home!
+        </button>
+      </NavLink>
+      <img src="./pet.png" alt="Pet Name Finder logo" className="logo"></img>
+      <h1 className="header-title">Pet Name Finder</h1>
+      <div className="button-container">
+        <NavLink to="/liked-names">
+          <button data-cy="view-liked-button" className="view-liked-button">
+            ⭐️ View Liked Names ⭐️
+          </button>
+        </NavLink>
+        {!loggedIn && (
+          <NavLink to="/login">
+            <button data-cy="login-button" className="login-button">
+              Login
+            </button>
+          </NavLink>
+        )}
+        {loggedIn && (
+          <button
+            data-cy="login-button"
+            className="login-button"
+            onClick={(event) => logoutUser(event)}
+          >
+            Logout
+          </button>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Header;
 
