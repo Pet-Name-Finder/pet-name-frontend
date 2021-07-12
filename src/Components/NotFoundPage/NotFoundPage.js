6648d5a29f0c47ac27d2763f12156faa0dc7b048
Zach Johnson <zachjjohns@gmail.com>
2021-07-11 20:41:34 -0500

    Update NotFoundPage styles

--- src/Components/NotFoundPage/NotFoundPage.js
+++ src/Components/NotFoundPage/NotFoundPage.js
@@ -4,12 +4,10 @@ import './NotFoundPage.css';
 
 const NotFoundPage = () => {
   return (
-    <>
+    <div className='not-found-container'>
+      <img className='error-img' src="404_error_penguin.jpeg"  alt="page not found with penguin holding 404 sign" style={{textAlign:"center"}} />
       <NavLink to='/'><button data-cy='home-button' className='home-button'>Take Me Back Home!</button></NavLink>
-      <div>
-        <img className='error-img' src="404_error_penguin.jpeg"  alt="page not found with penguin holding 404 sign" style={{textAlign:"center"}} />
-      </div>
-    </>
+    </div>
   )
 }
 
