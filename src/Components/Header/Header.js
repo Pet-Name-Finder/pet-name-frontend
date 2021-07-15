c28dfae92150e4f6985dbfbe08c1780b0c8ba0bb
Zach Johnson <zachjjohns@gmail.com>
2021-07-15 17:31:33 -0500

    Adjust header CSS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,17 +13,19 @@ const Header = ({ loggedIn, logoutUser }) => {
       </div>
       <NavLink to='/'><img src="./pet.png" alt="Pet Name Finder logo" className="logo"></img></NavLink>
       <h1 className='header-title'>Pet Name Finder</h1>
-      <NavLink to='/liked-names'><button data-cy='view-liked-button' className='view-liked-button'>⭐️ View Liked Names ⭐️</button></NavLink>
-      {!loggedIn &&
-      <NavLink to='/login'><button data-cy='login-button' className='login-button'>Login</button></NavLink>
-      }
-      {loggedIn &&
-      <button
-        data-cy='logout-button'
-        className='login-button'
-        onClick={(event) => logoutUser(event)}
-      >Logout</button>
-      }
+      <div className="button-container">
+        <NavLink to='/liked-names'><button data-cy='view-liked-button' className='view-liked-button'>⭐️ View Liked Names ⭐️</button></NavLink>
+        {!loggedIn &&
+        <NavLink to='/login'><button data-cy='login-button' className='login-button'>Login</button></NavLink>
+        }
+        {loggedIn &&
+        <button
+          data-cy='logout-button'
+          className='login-button'
+          onClick={(event) => logoutUser(event)}
+        >Logout</button>
+        }
+      </div>
     </header>
   );
 };
