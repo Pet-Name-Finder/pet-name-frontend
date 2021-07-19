7665034234016e86f1758246fee77bd2c2655aa2
Ryan Miller <ryandmiller3521@gmail.com>
2021-07-19 13:24:06 -0600

    Remove buttons

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -17,9 +17,7 @@ class Home extends Component {
     console.log(this.props);
     return (
       <div className="home">
-        <NavLink to="/all-packs"><button data-cy="view-packs-btn" className="main-btn view-packs-btn">View Your Packs</button></NavLink>
-        <button data-cy="start-pack-btn" className="main-btn start-pack-btn">Start New Pack</button>
-        <button data-cy="start-lone-btn" className="main-btn start-lone-btn">Start as a Lone Wolf</button>
+        <NavLink to="/all-packs"><button data-cy="view-packs-btn" className="main-btn view-packs-btn">Start Voting!</button></NavLink>
       </div>
     );
   }
