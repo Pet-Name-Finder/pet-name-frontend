d5f737df76e839f09368a1c4e382d90c7587b6a4
EllieAzaveda <elliemiller66@icloud.com>
2021-07-20 14:42:01 -0600

    Add mock data

---
+++ cypress/fixtures/mockUserData.js
@@ -0,0 +1,25 @@
+const mockUserData = {
+    "users": [
+        {
+            "email": "PatrickRoy@email.com",
+            "id": 1,
+            "likedNames": [
+                "Frank", "Bandit", "Bowser", "Daisy", "Gouda", "Mille"
+            ]
+        },
+        {
+            "email": "JoeSakic@email.com",
+            "id": 2,
+            "likedNames": [
+                "Daisy", "Gouda"
+            ]
+        },
+        {
+            "email": "random@email.com",
+            "id": 3,
+            "likedNames": [
+                "Blue", "Gouda", "Bouhda", "Daisy", "Mille"
+            ]
+        }
+    ]
+}
