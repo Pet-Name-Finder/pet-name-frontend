ffd223dd1c7b8c965961facba12ace796f3c4089
EllieAzaveda <elliemiller66@icloud.com>
2021-07-20 20:06:42 -0600

    Fix mock data

--- cypress/fixtures/mockNamesData.js
+++ cypress/fixtures/mockNamesData.js
@@ -1,64 +1,62 @@
-const mockNamesData = {
-    "names": [
-        {
-            "name": "Frank",
-            "type": "Dog",
-            "gender": "male"
-        },
-        {
-            "name": "Bandit",
-            "type": "Dog",
-            "gender": "male"
-        },
-        {
-            "name": "Bowser",
-            "type": "Dog",
-            "gender": "male"
-        },
-        {
-            "name": "Daisy",
-            "type": "Cat",
-            "gender": "female"
-        },
-        {
-            "name": "Gouda",
-            "type": "Cat",
-            "gender": "none"
-        },
-        {
-            "name": "Blue",
-            "type": "Cat",
-            "gender": "none"
-        },
-        {
-            "name": "Mille",
-            "type": "other",
-            "gender": "none"
-        },
-        {
-            "name": "Bouhda",
-            "type": "other",
-            "gender": "none"
-        },
-        {
-            "name": "Abby",
-            "type": "Dog",
-            "gender": "female"
-        },
-        {
-            "name": "Gypsy",
-            "type": "Dog",
-            "gender": "female"
-        },
-        {
-            "name": "Luke",
-            "type": "Dog",
-            "gender": "male"
-        },
-        {
-            "name": "Yoshi",
-            "type": "other",
-            "gender": "none"
-        },
-    ]
-}
+const mockNamesData = [
+  {
+      "name": "Frank",
+      "type": "Dog",
+      "gender": "male"
+  },
+  {
+      "name": "Bandit",
+      "type": "Dog",
+      "gender": "male"
+  },
+  {
+      "name": "Bowser",
+      "type": "Dog",
+      "gender": "male"
+  },
+  {
+      "name": "Daisy",
+      "type": "Cat",
+      "gender": "female"
+  },
+  {
+      "name": "Gouda",
+      "type": "Cat",
+      "gender": "none"
+  },
+  {
+      "name": "Blue",
+      "type": "Cat",
+      "gender": "none"
+  },
+  {
+      "name": "Mille",
+      "type": "other",
+      "gender": "none"
+  },
+  {
+      "name": "Bouhda",
+      "type": "other",
+      "gender": "none"
+  },
+  {
+      "name": "Abby",
+      "type": "Dog",
+      "gender": "female"
+  },
+  {
+      "name": "Gypsy",
+      "type": "Dog",
+      "gender": "female"
+  },
+  {
+      "name": "Luke",
+      "type": "Dog",
+      "gender": "male"
+  },
+  {
+      "name": "Yoshi",
+      "type": "other",
+      "gender": "none"
+  }
+]
