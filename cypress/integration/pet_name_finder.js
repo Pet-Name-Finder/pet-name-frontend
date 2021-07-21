7f0d59ab2b35958d79b308281ee7ba0c2b1a0bce
EllieAzaveda <elliemiller66@icloud.com>
2021-07-20 19:36:51 -0600

    Add logout button test

--- cypress/integration/pet_name_finder.js
+++ cypress/integration/pet_name_finder.js
@@ -42,73 +42,34 @@ describe('Show pages of Pet Name Finder App', () => {
 
   });
 
-  it('Should display the Home Page after logging in', () => {
+  it('Should be able to login with matching email address', () => {
     cy.get('.login').find('[data-cy=email-input]').type('boris_walker@parisian.io')
       .get('.login').find('[data-cy=login-now-btn]').click()
       .get('header').find('[data-cy=view-liked-button]').should('be.visible')
       .get('.home').find('[data-cy=start-voting-btn]').should('contain', 'Start Voting!')
   });
 
-  // it('Should display the default page start voting button after logging in', () => {
-  //   cy.get('.home').find('.view-packs-btn').should('be.visible')
-  //     .get('.home').find('[data-cy=view-packs-btn]').should('contain', 'View Your Packs')
-  //     .get('.home').find('.start-pack-btn').should('be.visible')
-  //     .get('.home').find('[data-cy=start-pack-btn]').should('contain', 'Start New Pack')
-  //     .get('.home').find('.start-lone-btn').should('be.visible')
-  //     .get('.home').find('[data-cy=start-lone-btn]').should('contain', 'Start as a Lone Wolf')
-  // });
-
-});
-
-describe('Login and Logout of Pet Name Finder App', () => {
-
-  // beforeEach(() => {
-  //   cy.fixture('mockUserData.json')
-  //   .then(mockData => {
-  //     cy.intercept('GET', 'https://', {
-  //       statusCode: 201,
-  //       delay: 100,
-  //       body: mockData
-  //     })
-  //   })
-  //   cy.visit('http://localhost:3000')
-  // });
-
-  it('Should be able to login with matching email address', () => {
-    // test for input box and submit button
-    // test for typing
-  });
-
   it('Should not be able to login and be alerted of wrong email', () => {
-    // test for error handling - alerts
-    // wrong input type (not an email)
-    // non-matching email
+    const stub = cy.stub()
+
+    // cy.on ('window:alert', stub)
+    // cy.get('.login').find('[data-cy=email-input]').type('bored_runner@america.co')
+    //   .get('.login').find('[data-cy=login-now-btn]').click()
+    //   .then(() => {
+    //     expect(stub.getCall(0)).to.be.calledWith('Sorry no account found!')
+    //   })
   });
 
   it('Should display logout instead of login button after successful login', () => {
-    // test button contains
+    cy.get('.login').find('[data-cy=email-input]').type('boris_walker@parisian.io')
+      .get('.login').find('[data-cy=login-now-btn]').click()
+      .get('header').find('[data-cy=logout-button]').should('contain', "Logout")
   });
 
   it('Should be able to logout', () => {
     // After click => currentUser state is changed
     // After logout => Button goes back to Login
   });
-
-});
-
-describe('User Interaction with names data on Pet Name Finder App', () => {
-
-  // beforeEach(() => {
-  //   cy.fixture('mockNamesData.json')
-  //   .then(mockData => {
-  //     cy.intercept('GET', 'https://', {
-  //       statusCode: 201,
-  //       delay: 100,
-  //       body: mockData
-  //     })
-  //   })
-  //   cy.visit('http://localhost:3000')
-  // });
 
   it('Should be able to choose names', () => {
     // test for ability to like a name
