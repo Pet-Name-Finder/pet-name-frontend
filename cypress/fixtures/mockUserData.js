c41ecbf8f0693f67c6f916e8c5d6bd5fafa1f569
EllieAzaveda <elliemiller66@icloud.com>
2021-07-20 22:40:57 -0600

    Change liked names to objects

--- cypress/fixtures/mockUserData.js
+++ cypress/fixtures/mockUserData.js
@@ -1,5 +1,26 @@
 const mockUserData = {
     "email": "boris_walker@parisian.io",
     "id": 1,
-    "likedNames": ["Blue", "Gouda", "Bouhda", "Daisy", "Mille"]
+    "likedNames": [
+        {
+            "id": 15,
+            "name": "Blue"
+        },
+        {
+            "id": 14,
+            "name": "Gouda"
+        },
+        {
+            "id": 17,
+            "name": "Bouhda"
+        },
+        {
+            "id": 13,
+            "name": "Daisy"
+        },
+        {
+            "id": 16,
+            "name": "Mille"
+        }
+    ]
 }
