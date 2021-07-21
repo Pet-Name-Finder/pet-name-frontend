f987a5dfb59858c88fe84dd27df610ab3e37925c
EllieAzaveda <elliemiller66@icloud.com>
2021-07-21 10:07:14 -0600

    Add data cy tags

--- src/Components/LikedNames/LikedNames.js
+++ src/Components/LikedNames/LikedNames.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const LikedNames = ({ likedNames }) => {
   const mappedNames = likedNames.map((name) => {
-    return <li key={name.id}>{name.name}</li>;
+    return <li key={name.id} data-cy="name">{name.name}</li>;
   });
 
   return (
