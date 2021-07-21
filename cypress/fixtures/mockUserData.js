15d431542851a29622311e2fc9c16678be7a5064
EllieAzaveda <elliemiller66@icloud.com>
2021-07-21 09:57:31 -0600

    Add mock user data

---
+++ cypress/fixtures/mockUserData.js
@@ -0,0 +1,26 @@
+const mockUserData = {
+    "email": "boris_walker@parisian.io",
+    "id": 1,
+    "likedNames": [
+        {
+            "id": 15,
+            "name": "Blue"
+        },
+        {
+            "id": 14,
+            "name": "Gouda"
+        },
+        {
+            "id": 17,
+            "name": "Bouhda"
+        },
+        {
+            "id": 13,
+            "name": "Daisy"
+        },
+        {
+            "id": 16,
+            "name": "Mille"
+        }
+    ]
+}
